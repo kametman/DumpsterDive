239d4a93a46fa9d77fdae521515dbc3f96f6f7e3
Ray Cooley <r0cool01@gmail.com>
2017-08-02 21:05:22 -0400

    command line arguments
    Added support for command line arguments.
    Refactored code.
    Updated README.md

--- Program.cs
+++ Program.cs
@@ -14,72 +14,160 @@ namespace DumpsterDive
 
         static void Main(string[] args)
         {
+            char sizeType = ' ';
+            int chunks = 0;
+            bool? includeUpper = null;
+            bool? includeLower = null;
+            bool? includeNumbers = null;
+            bool? includeSymbols = null;
+            bool wait = true;
+
             Console.WriteLine("DumpsterDive Garbage File Generator");
-            Console.WriteLine("File size type (KB/MB)? ");
-            var sizeType = Console.ReadLine().ToUpper()[0];
-            if (sizeType.Equals("K") || sizeType.Equals("M"))
+
+            if (args.Length > 0)
             {
-                Console.Write($"File size (in {sizeType}B)? ");
-                var sizeInput = Console.ReadLine();
-
-                var chunks = 0;
-                if (int.TryParse(sizeInput, out chunks) && (sizeType.Equals('K') || sizeType.Equals('M')))
+                Console.WriteLine("Parsing command line parameters...");
+                foreach (var arg in args)
                 {
-                    var fileSize = 1024 * chunks;
-                    if (sizeType.Equals('m')) { fileSize *= 1024; }
+                    switch (arg[1])
+                    {
+                        case 't': sizeType = arg.ToUpper()[2]; break;
+                        case 's': int.TryParse(arg.Substring(2), out chunks); break;
+                        case 'c':
+                            foreach (char c in arg.Substring(2))
+                            {
+                                if (c.Equals('A')) { includeUpper = true; }
+                                if (c.Equals('a')) { includeLower = true; }
+                                if (c.Equals('1')) { includeNumbers = true; }
+                                if (c.Equals('!')) { includeSymbols = true; }
+                            }
+                            break;
+                        case 'n': wait = false; break;
+                        default: Console.WriteLine($"\tInvalid argument: {arg}"); break;
+                    }
+                }
+            }
 
-                    var fileChars = new List<string>();
+            if (sizeType.Equals(' '))
+            {
+                try
+                {
+                    Console.WriteLine("File size type (KB/MB)? ");
+                    sizeType = Console.ReadLine().ToUpper()[0];
+                }
+                catch { sizeType = ' '; }
+            }
+            if (chunks == 0 && (sizeType.Equals('K') || sizeType.Equals('M')))
+            {
+                try
+                {
+                    Console.Write($"File size (in {sizeType}B)? ");
+                    var sizeInput = Console.ReadLine();
+
+                    chunks = 0;
+                    int.TryParse(sizeInput, out chunks);
+                }
+                catch { chunks = 0; }
+            }
+            if (!includeUpper.HasValue && !includeLower.HasValue && !includeNumbers.HasValue && !includeSymbols.HasValue)
+            {
+                try
+                {
                     Console.Write("Include all characters (y/n)? ");
                     var charInput = Console.ReadLine().ToLower()[0];
                     if (charInput.Equals('n'))
                     {
                         Console.WriteLine("Include upper-case characters (y/n)? ");
                         charInput = Console.ReadLine().ToLower()[0];
-                        if (charInput.Equals('y')) { fileChars.Add(UPPER_CASE); }
+                        includeUpper = charInput.Equals('y');
 
                         Console.WriteLine("Include lower-case characters (y/n)? ");
                         charInput = Console.ReadLine().ToLower()[0];
-                        if (charInput.Equals('y')) { fileChars.Add(LOWER_CASE); }
+                        includeLower = charInput.Equals('y');
 
                         Console.WriteLine("Include numbers (y/n)? ");
                         charInput = Console.ReadLine().ToLower()[0];
-                        if (charInput.Equals('y')) { fileChars.Add(NUMBERS); }
+                        includeNumbers = charInput.Equals('y');
 
                         Console.WriteLine("Include symbols (y/n)? ");
                         charInput = Console.ReadLine().ToLower()[0];
-                        if (charInput.Equals('y')) { fileChars.Add(SYMBOLS); }
+                        includeSymbols = charInput.Equals('y');
                     }
-                    else { fileChars.AddRange(new string[] { UPPER_CASE, LOWER_CASE, NUMBERS, SYMBOLS }); }
-
-                    if (fileChars.Count() > 0)
+                    else
                     {
-                        var randGen = new Random();
-                        var fileName = Guid.NewGuid().ToString();
-                        Console.WriteLine($"Generating {chunks}{sizeType}B garbage file...");
-                        Console.WriteLine($"File name: {fileName}");
-
-                        var n = -1;
-                        using (var sw = new StreamWriter(fileName))
-                        {
-                            for (int i = 0; i < fileSize; i++)
-                            {
-                                n = randGen.Next(fileChars.Count());
-                                sw.Write(fileChars[n][randGen.Next(fileChars[n].Length)]);
-                            }
-
-                            sw.Close();
-                        }
-
-                        Console.WriteLine($"Garbage file '{fileName}' completed ({chunks}{sizeType}B).");
+                        includeUpper = charInput.Equals('y');
+                        includeLower = charInput.Equals('y');
+                        includeNumbers = charInput.Equals('y');
+                        includeSymbols = charInput.Equals('y');
                     }
-                    else { Console.WriteLine("No characters selected! Exiting program..."); }
                 }
-                else { Console.WriteLine("Invalid file size! Exiting program..."); }
+                catch { }
             }
-            else { Console.WriteLine("Invalid file size type! Exiting program..."); }
 
-            Console.WriteLine("Press any key to exit");
-            Console.ReadKey();
+            if (ValidateParameters(sizeType, chunks, includeUpper ?? false, includeLower ?? false, includeNumbers ?? false, includeSymbols ?? false))
+            {
+                var charSets = new List<string>();
+                if (includeLower.HasValue && includeLower.Value) { charSets.Add(UPPER_CASE); }
+                if (includeUpper.HasValue && includeUpper.Value) { charSets.Add(LOWER_CASE); }
+                if (includeNumbers.HasValue && includeNumbers.Value) { charSets.Add(NUMBERS); }
+                if (includeSymbols.HasValue && includeSymbols.Value) { charSets.Add(SYMBOLS); }
+
+                GenerateGarbage(sizeType, chunks, charSets);
+            }
+            else { Console.WriteLine("Invalid parameters were found. File not generated."); }
+
+            if (wait)
+            {
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            }
+        }
+
+        static bool ValidateParameters(char sizeType, int chunks, bool upper, bool lower, bool numbers, bool symbols)
+        {
+            bool isValid = true;
+            if (!sizeType.Equals('M') && !sizeType.Equals('K'))
+            {
+                Console.WriteLine("\tInvalid file size type!");
+                isValid = false;
+            }
+            if (chunks < 1)
+            {
+                Console.WriteLine("\tInvalid file size!");
+                isValid = false;
+            }
+            if (!upper && !lower && !numbers && !symbols)
+            {
+                Console.WriteLine("\tNo character sets selected!");
+                isValid = false;
+            }
+
+            return isValid;
+        }
+
+        static void GenerateGarbage(char sizeType, int chunks, IList<string> charSets)
+        {
+            var randGen = new Random();
+            var fileName = Guid.NewGuid().ToString();
+            var fileSize = 1024 * chunks;
+            if (sizeType.Equals('M')) { fileSize *= 1024; }
+
+            Console.WriteLine($"Generating {chunks}{sizeType}B garbage file...");
+            Console.WriteLine($"File name: {fileName}");
+
+            var n = -1;
+            using (var sw = new StreamWriter(fileName))
+            {
+                for (int i = 0; i < fileSize; i++)
+                {
+                    n = randGen.Next(charSets.Count());
+                    sw.Write(charSets[n][randGen.Next(charSets[n].Length)]);
+                }
+
+                sw.Close();
+            }
+
+            Console.WriteLine($"Garbage file '{fileName}' completed ({chunks}{sizeType}B).");
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # DumpsterDive
 simple application to generate garbage files in a variety of sizes.
+
+## Overview
+DumpsterDive is a .Net console application that will generate a randomized ASCII file.  A menu within the application is used to control the parameters of the file generated:
+
+* File size (in either KB or MB)
+* Characters used
+  * Upper case letters
+  * Lower case letters
+  * Numbers
+  * Symbols
+
+### Command Line Arguments
+DumpsterDive also allows for command line parameters to generate the file.  Any parameters not passed via the command line will cause a prompt to appear in console window.
+* -t: Determines whether file generated is kilobytes or megabytes
+* -s: Determines size of file.
+* -c: Determines character sets used:
+  * A - upper case letters
+  * a - lower case letters
+  * 1 - numbers
+  * ! - symbols
+* -n: Will cause the console window to close automatically after file is generated.
+
+Example "-tk -s100 -sAa" will generate a 100 kilobyte file filled with upper and lowercase letters.
+
